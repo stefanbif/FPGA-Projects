87efaf52770548c89749e2e7b77c606799db24ea
Stefan Bifolchi <73149111+stefanbif@users.noreply.github.com>
2022-11-10 14:06:39 -0500

    Update ADDTEST_CHANGES.v

--- Lab_4_Counter/ADDTEST_CHANGES.v
+++ Lab_4_Counter/ADDTEST_CHANGES.v
@@ -9,26 +9,25 @@
 module ADDTEST(
 
 	//////////// CLOCK //////////
-//	input 		          		ADC_CLK_10,
+
 	input 		          		CLOCK2_50,  //sh change the name here according to system builder ADDTEST version
-//	input 		          		MAX10_CLK2_50,
+
 
 	//////////// SEG7 //////////
 	output		     [6:0]		HEX0,  //sh chenged to 6:0
 	output		     [6:0]		HEX1,
-//	output		     [7:0]		HEX2,
-//	output		     [7:0]		HEX3,
-//	output		     [7:0]		HEX4,
-//	output		     [7:0]		HEX5,
+	output		     [6:0]		HEX2,
+	output		     [6:0]		HEX3,
+
 
 	//////////// KEY //////////
 	input 		     [1:0]		KEY,
 
 	//////////// LED //////////
-	output		     [5:0]		LEDR, // only use the least 3 bits. // sh from 9:0 to 5:0
+	output		     [6:0]		LEDR, 
 
 	//////////// SW //////////
-	input 		     [2:0]		SW  // only use the least 3 bits
+	input 		     [6:0]		SW  
 );
 
 
@@ -36,11 +35,12 @@ module ADDTEST(
 //=======================================================
 //  REG/WIRE declarations
 //=======================================================
-wire A_d, B_d, C_d; // temp signal to connect synchronizer 
-					// output to the input of the adder
-wire sum_temp, carry_temp; // temp signal to connect output
-							// of the adder to the 7-segment
-
+// temp signal to connect synchronizer output to the input of the counter
+	wire en_d, PL0_d, PL1_d, PL2_d,PL3_d, PL4_d, PL5_d; 
+// temp signal to connect output of the counter to the 7-segment
+	wire preload0_temp, preload1_temp, disp0_temp, disp1_temp;
+ // temp signal to connect output of the counter to the 7-segment
+	wire preload0_temp, preload1_temp, disp0_temp, disp1_temp;
 
 //=======================================================
 //  Structural coding
@@ -51,20 +51,48 @@ sync u_sync1(
 	.rst_n(KEY[0]),
 	.clk(CLOCK2_50),
 	.in(SW[0]),
-	.out(A_d)
+	.out(en_d)
 ) ; 	
 sync u_sync2(
 	.rst_n(KEY[0]),
 	.clk(CLOCK2_50),
 	.in(SW[1]),
-	.out(B_d)
+	.out(PL0_d)
 ) ; 	
 sync u_sync3(
 	.rst_n(KEY[0]),
 	.clk(CLOCK2_50),
 	.in(SW[2]),
-	.out(C_d)
+	.out(PL1_d)
+) ; 
+sync u_sync4(
+	.rst_n(KEY[0]),
+	.clk(CLOCK2_50),
+	.in(SW[3]),
+	.out(PL2_d)
 ) ; 	
+sync u_sync5(
+	.rst_n(KEY[0]),
+	.clk(CLOCK2_50),
+	.in(SW[4]),
+	.out(PL3_d)
+) ; 
+
+sync u_sync6(
+	.rst_n(KEY[0]),
+	.clk(CLOCK2_50),
+	.in(SW[5]),
+	.out(PL4_d)
+) ; 
+
+sync u_sync7(
+	.rst_n(KEY[0]),
+	.clk(CLOCK2_50),
+	.in(SW[6]),
+	.out(PL5_d)
+) ; 
+	
+	
 // full adder
 adder u_adder(
 	.rst_n(KEY[0]),
